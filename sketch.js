58271ddeb9441e1ad74cb93f43a35afbef46c51f
kaue0112 <silvakaue567@gmail.com>
2024-06-12 09:50:14 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background("white");
+    textAlign (CENTER, CENTER) 
+    textSize(60)
+    fill ("black")
+    let palavra = "super homem" 
+    text(palavra, 200,200)
+    
+  }
